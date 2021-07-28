c7086288713390b02754c77ce2c7eb060222f609
he1l0world <hellochen19970916@gmail.com>
2021-07-28 11:48:10 -0700

    add testcase for stacklinkedlist

--- src/test/java/com/dawson/StackLinkedListTest.java
+++ src/test/java/com/dawson/StackLinkedListTest.java
@@ -1,5 +1,9 @@
 package com.dawson;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 /**
 * The StackLinkedList test program will create several testcases to test
 * the StackLinkedList library
@@ -9,5 +13,9 @@ package com.dawson;
 * @since   2021-07-25 
 */
 public class StackLinkedListTest {
-    
+    @Test
+    public void test_empty(){
+        StackLinkedList stack = new StackLinkedList();
+        assertTrue(stack.isEmpty());
+    }
 }
