e42faf1e74afcb4e24ed7623a7f4d7e85693e229
he1l0world <hellochen19970916@gmail.com>
2021-08-06 00:58:34 -0700

    add testcases for StackLinkedList

--- src/test/java/com/dawson/StackLinkedListTest.java
+++ src/test/java/com/dawson/StackLinkedListTest.java
@@ -1,6 +1,7 @@
 package com.dawson;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -17,5 +18,32 @@ public class StackLinkedListTest {
     public void test_empty(){
         StackLinkedList stack = new StackLinkedList();
         assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    public void test_push_pop(){
+        StackLinkedList stack = new StackLinkedList();
+        stack.push(1);
+        assertEquals(1, stack.pop());
+    }
+    
+    @Test
+    public void test_size(){
+        StackLinkedList stack = new StackLinkedList();
+        stack.push(1);
+        stack.push(3);
+        stack.push(5);
+        assertEquals(3, stack.size());
+    }
+
+    @Test
+    public void test_zeroToNine(){
+        StackLinkedList stack = new StackLinkedList();
+        for(int index = 0; index < 10; index++){
+            stack.push(index);
+        }
+        for(int index = 9; index >=0; index--){
+            assertEquals(index, stack.pop());
+        }
     }
 }
